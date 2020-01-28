3c840187720b996a6edf5b6b814278a0b1eb8fbf
Androix777 <androix77@gmail.com>
2020-01-28 09:32:28 +0500

    Улучшен код генератора

--- Assets/Scripts/Generator.cs
+++ Assets/Scripts/Generator.cs
@@ -6,13 +6,13 @@ public class Generator : MonoBehaviour
 {
     public GameObject player;
     private GameObject room;
-    int[] MAX_ROOMS = new int[] {3, 1};
+    private int levelNum;
+    private LevelName levelName;
     void Start()
     {
-        room = Instantiate(Resources.Load("Prefabs/Rooms/Normal/Room1") as GameObject, gameObject.transform.position, Quaternion.identity);
-        room.transform.parent = gameObject.transform;
-        player.transform.position = room.transform.Find("Spawn").transform.position;
-        player.transform.parent = room.transform;
+        CreateRoom("Prefabs/Rooms/Normal/Room1");
+        levelNum = 1;
+        levelName = LevelName.Start;
     }
 
     void Update()
@@ -26,12 +26,29 @@ public class Generator : MonoBehaviour
 
     public void NextLevel()
     {
+
+        CreateRoom(CalcChance(levelNum, levelName));
+        levelNum++;
+    }
+
+    void CreateRoom(string path)
+    {
         player.transform.parent = gameObject.transform;
         Destroy(room);
-        
-        room = Instantiate(Resources.Load("Prefabs/Rooms/Normal/Room2") as GameObject, gameObject.transform.position, Quaternion.identity);
+        room = Instantiate(Resources.Load(path) as GameObject, gameObject.transform.position, Quaternion.identity);
         room.transform.parent = gameObject.transform;
         player.transform.position = room.transform.Find("Spawn").transform.position;
         player.transform.parent = room.transform;
+    }
+
+    string CalcChance(int level, LevelName name)
+    {
+        switch(name)
+        {
+            case LevelName.Start:
+                return "Prefabs/Rooms/Normal/Room"+Random.Range(2,4).ToString();
+            default:
+                return "No";
+        }
     }
 }
